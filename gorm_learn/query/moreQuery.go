3e7d89b7f764584f914100c6d124337725b44d37
Feynman <290154062@qq.com>
2024-01-14 22:25:00 +0800

    feat: 完善gorm 高级查询

--- gorm_learn/query/moreQuery.go
+++ gorm_learn/query/moreQuery.go
@@ -17,11 +17,23 @@ type User struct {
 }
 
 func QueryDataFunc(DB *gorm.DB) {
-	setMoreData(DB)
+	// 1.插入数据
+	// setMoreData(DB)
+	// 2.where 查询
 	whereQuery(DB)
+	// 3.结构体查询
+	structQuery(DB)
+	// 4.使用map查询 不会过滤零值
+	mapQuery(DB)
+	// 5.not条件 和where中的not等价
+	notQuery(DB)
+	// 6.Or条件
+	orQuery(DB)
+	// 7.Select 选择字段 
+	selectQuery(DB)
 }
 
-/* 插入一些数据 */
+/* 1.插入一些数据 */
 func setMoreData(DB *gorm.DB){
 	var users []User
 	// 删除所有数据
@@ -44,16 +56,100 @@ func ptrString(email string) *string {
   return &email
 }
 
-/* where  等价于sql语句中的where*/
+/* 2.where  等价于sql语句中的where*/
 func whereQuery(DB *gorm.DB){
 	var users []User
 	// 查询用户名是枫枫的
 	DB.Where("name=?","枫枫").Find(&users)
-	printQueryData(users)
-
+	printQueryData(users,"查询用户名是枫枫的:")
+	// 查询用户名不是枫枫
+	DB.Where("name <> ?","枫枫").Find(&users)
+	printQueryData(users,"查询用户名不是枫枫:")
+	// 查询用户名包含 如燕，李元芳的
+	DB.Where("name in ?",[]string{"如燕", "李元芳"}).Find(&users)
+	printQueryData(users,"查询用户名包含 如燕，李元芳的:")
+	// 查询姓李的
+	DB.Where("name like ?","李%").Find(&users)
+	printQueryData(users,"查询姓李的:")
+	// 查询年龄大于23，是qq邮箱的
+	DB.Where("age > ? and email like ?","23","%@qq.com").Find(&users)
+	printQueryData(users,"查询年龄大于23，是qq邮箱的:")
+	// 查询是qq邮箱的，或者是女的
+	DB.Where("gender = ? or email like ?",false,"%@qq.com").Find(&users)
+	printQueryData(users,"查询是qq邮箱的，或者是女的:")
 }
 
-func printQueryData(list []User){
+/* 3.使用结构体查询  会过滤零值*/
+func structQuery(DB *gorm.DB){
+	var users []User
+	DB.Where(&User{Name: "李元芳",Age: 0}).Find(&users)
+	
+	printQueryData(users,"使用结构体查询:")
+}
+
+/* 4.使用map查询 不会过滤零值 */
+func mapQuery(DB *gorm.DB){
+	var users []User
+	DB.Where(map[string]any{"name":"李元芳","age":0}).Find(&users)
+	printQueryData(users,"使用map查询 不会过滤零值:")
+}
+
+/* 5.not条件 和where中的not等价*/
+func notQuery(DB *gorm.DB){
+	// 排除年龄大于23的
+	var users []User
+	DB.Not("age>23").Find(&users)
+	printQueryData(users,"not条件,排除年龄大于23的:")
+}
+
+/* 6.Or条件  和where中的or等价*/
+func orQuery(DB *gorm.DB){
+	var users []User
+	DB.Or("gender = ?",false).Or(" email like ?","%@qq.com").Find(&users)
+	printQueryData(users,"Or条件,查询是qq邮箱的，或者是女的:")
+}
+
+/* 7.Select 选择字段 */
+func selectQuery(DB *gorm.DB){
+	var users []User
+	DB.Select("name", "age").Find(&users)
+	printQueryData(users,"Select 选择字段,没有被选中，会被赋零值:")
+
+	// 可以使用扫描Scan，将选择的字段存入另一个结构体中
+	type selectUser struct {
+		Name string
+		Age  int
+	}
+	users = []User{}
+	var sUsers []selectUser
+	DB.Select("name", "age").Find(&users).Scan(&sUsers)
+	fmt.Println(sUsers)
+	// 上面写法写也是可以的，不过最终会查询两次，还是不这样写
+	// 下面这样写就只查询一次了
+	DB.Model(&User{}).Select("name", "age").Scan(&sUsers)
+	fmt.Println(sUsers)
+	
+	// 还可以这样
+	DB.Table("t_user").Select("name", "age").Scan(&sUsers)
+	fmt.Println(sUsers)
+
+	// Scan是根据column列名进行扫描的
+	type scanUser struct {
+		Name123 string `gorm:"column:name"`
+		Age  int
+	}
+	var colUsers []scanUser
+	DB.Table("t_user").Select("name", "age").Scan(&colUsers)
+	fmt.Println(colUsers)
+}
+
+
+func printQueryData(list []User,desc string){
+	fmt.Println(desc+"=================")
+	if(len(list)<=0){
+		fmt.Println(list)
+		return
+	}
 	for _, item := range list{
 		data,_ := json.Marshal(item)
 		fmt.Println(string(data))
